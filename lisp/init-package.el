0a978811c55983b2286541d2fee202d58160935b
谢国辉 <duanhunyiye@163.com>
2020-12-04 00:43:06 +0800

    drag stuff up/down

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -1,2 +1,11 @@
 (use-package restart-emacs)
+
+(use-package hungry-delete
+  :bind (("C-c DEL" . hungry-delete-backward))
+  :bind (("C-c d" . hungry-delete-forward)))
+
+(use-package drag-stuff
+  :bind (("<M-up>" . drag-stuff-up))
+  ("<M-down>" . drag-stuff-down))
+
 (provide 'init-package)
